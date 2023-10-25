d63f8cf827ada14b2d1ac318db8bed16cdd0e78d
win5948 <win5948@gmail.com>
2023-10-25 17:18:39 +0900

    스캐너 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,15 @@
 package com.ll;
 
-class App {
+import java.util.Scanner;
+
+public class App {
     void run() {
-        System.out.println("프로그램 실행");
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+
+        System.out.printf("입력하신 명령 : %s\n" , cmd);
     }
 }
